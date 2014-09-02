2efb8bf5079b48757c3f03c5967bd69342198b72
Dooley <nsewolo@gmail.com>
2014-09-01 21:39:38 -0400

    test infra setup

--- JavaScript/design_patterns/src/factory/factory.js
+++ JavaScript/design_patterns/src/factory/factory.js
@@ -1,3 +1,9 @@
-function Car() {
+(function () {
 
-}
+    'use strict';
+
+    function Car() {
+
+    }
+
+})();
---
+++ JavaScript/design_patterns/tests/factory/factory_test.js
@@ -0,0 +1,19 @@
+//= require mocha
+
+var assert = require("assert");
+var should = require('should');
+var Factory = require('../../src/factory/factory');
+
+suite('Car Maker', function () {
+    suite('Create a new car', function () {
+        describe('car factory', function () {
+
+            it('should create a new car', function () {
+                assert.equal(true, true);
+            });
+        });
+
+    });
+});
+
+
