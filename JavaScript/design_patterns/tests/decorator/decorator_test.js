5f956614e347bb883869ee321d38b5da490ab01f
Dooley <nsewolo@gmail.com>
2014-09-08 19:23:57 -0400

    beter with only on assert by test

--- JavaScript/design_patterns/tests/decorator/decorator_test.js
+++ JavaScript/design_patterns/tests/decorator/decorator_test.js
@@ -7,21 +7,33 @@ describe('Factory tests', function () {
     'use strict';
 
     describe('Decorate the computer', function () {
+        var macBook = new computer.Computer('MacBook Pro', 990, 13);
         it('should return the cost of the computer', function () {
-            var macBook = new computer.Computer('MacBook Pro', 990, 13);
             assert.equal(macBook.getCost(), '990');
+        });
+
+        it('should return the model of the computer', function () {
             assert.equal(macBook.getModel(), 'MacBook Pro');
+        });
+
+        it('should return the size of the computer', function () {
             assert.equal(macBook.getSize(), '13');
         });
     });
 
     describe('Decorator off a computer', function () {
-        it('should decorate computer with memory and hard drive price', function () {
-            var macBook = new computer.Computer('MacBook Pro', 990, 13);
-            var decorator = new decorator_module.Decorator();
+        var macBook = new computer.Computer('MacBook Pro', 990, 13);
+        var decorator = new decorator_module.Decorator();
+
+        it('should retrieve the basic price of the computer', function () {
             assert.equal(macBook.getCost(), 990);
+        });
+
+        it('should decorate computer with the memory price', function () {
             decorator.memory(macBook);
             assert.equal(macBook.getCost(), 1065);
+        });
+        it('should decorate computer with the hard drive price', function () {
             decorator.hardDrive(macBook);
             assert.equal(macBook.getCost(), 1315)
         });
