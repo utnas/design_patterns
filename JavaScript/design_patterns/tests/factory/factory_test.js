c5d5ae24306c55a59b6304da8f91b5bb644907ef
Dooley <nsewolo@gmail.com>
2014-09-08 20:56:22 -0400

    better with only on assert by test

--- JavaScript/design_patterns/tests/factory/factory_test.js
+++ JavaScript/design_patterns/tests/factory/factory_test.js
@@ -7,32 +7,47 @@ describe('Factory pattern', function () {
     'use strict';
 
     describe('Get options of a car_module', function () {
-        it('should return options of a car_module when the value is present', function () {
-            var ford = new car.Car({'model': 'Focus', 'version': '90-TDI'});
+
+        it('should return model of a car when the value is present', function () {
+            var ford = instantiateCar();
             assert.equal(ford.getOptions().model, 'Focus');
-            assert.equal(ford.getOptions().version, '90-TDI');
         });
-        it('should return undefined if no option provided', function () {
-            var ford = new car.Car('');
-            assert.equal(ford.getOptions().model, undefined);
-            assert.equal(ford.getOptions().version, undefined);
+
+        it('should return version of a car when the value is present', function () {
+            var ford = instantiateCar();
+            assert.equal(ford.getOptions().version, '90-TDI');
         });
     });
 
     describe('Car factory make a car ford with model fiesta', function () {
-        it('should make a ford fiesta_module', function () {
-            var factory = new carFactory.Factory();
-            var fiesta = factory.makeCar('FIESTA');
+        it('should return the model from a ford fiesta options', function () {
+            var fiesta = instantiateFactory().makeCar('FIESTA');
             assert.equal(fiesta.getOptions().model, 'Fiesta');
+        });
+
+        it('should return the version from a ford fiesta options', function () {
+            var fiesta = instantiateFactory().makeCar('FIESTA');
             assert.equal(fiesta.getOptions().version, '90-TDI');
         });
 
-        it('should make a car ford with model focus', function () {
-            var factory = new carFactory.Factory();
-            var focus = factory.makeCar('FOCUS');
+        it('should return the model of a car ford with model focus', function () {
+            var focus = instantiateFactory().makeCar('FOCUS');
             assert.equal(focus.getOptions().model, 'Focus');
+        });
+
+        it('should return the version of a car ford with model focus', function () {
+            var focus = instantiateFactory().makeCar('FOCUS');
             assert.equal(focus.getOptions().version, '230-TDI');
         });
     });
 
 });
+
+function instantiateCar(options) {
+    var option = options || {'model': 'Focus', 'version': '90-TDI'};
+    return new car.Car(option);
+}
+
+function instantiateFactory() {
+    return new carFactory.Factory();
+}
