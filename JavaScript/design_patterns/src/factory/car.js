fc0ec20d76503f501914345fe3ab47e869dc1973
Dooley <nsewolo@gmail.com>
2014-10-09 19:48:03 -0400

    minor changes

--- JavaScript/design_patterns/src/factory/car.js
+++ JavaScript/design_patterns/src/factory/car.js
@@ -5,7 +5,7 @@ exports.Car = function (options) {
     if (options !== undefined) {
         _options.model = options.model;
         _options.version = options.version;
-        _options.factoryYear = options.factoryYear;
+        _options.year = options.year;
     }
 
     this.getOptions = function () {
