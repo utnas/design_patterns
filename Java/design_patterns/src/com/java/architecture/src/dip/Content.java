bb3337287072f4bf0967802eee78495c9f9b9244
Dooley <nsewolo@gmail.com>
2014-11-20 00:51:45 -0500

    Dependency inversion principle training

--- Java/design_patterns/src/com/java/architecture/src/dip/Content.java
+++ Java/design_patterns/src/com/java/architecture/src/dip/Content.java
@@ -1,11 +1,11 @@
 package com.java.architecture.src.dip;
+// https://www.dotnetdojo.com/concevoir-des-applications-solid-lsp/
 
 public class Content implements IContent {
 
     private final String message;
 
     public Content(final String message) {
-
         this.message = message;
     }
 
