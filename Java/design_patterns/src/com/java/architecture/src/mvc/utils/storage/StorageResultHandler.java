2dc0c39aecd34cf55a05127080ca69cb5ffb1750
Dooley <nsewolo@gmail.com>
2015-01-01 18:15:40 -0500

    Abstration added to IStorage interface

--- Java/design_patterns/src/com/java/architecture/src/mvc/utils/storage/StorageResultHandler.java
+++ Java/design_patterns/src/com/java/architecture/src/mvc/utils/storage/StorageResultHandler.java
@@ -3,5 +3,5 @@ package com.java.architecture.src.mvc.utils.storage;
 import java.util.ArrayList;
 
 public interface StorageResultHandler<T> {
-    T handle(final Object subject, ArrayList store);
+    public T handle(final Object subject, ArrayList store);
 }
